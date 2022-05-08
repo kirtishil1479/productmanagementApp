b1db6616edc3628a2894d592c7a3ec19970a9ab9
saurabhnavghare <saurabhrn16@gmail.com>
2022-05-08 10:41:00 +0530

    PMA-5 added service layer

--- src/main/java/com/kjd/service/Saurabh_Service.java
+++ src/main/java/com/kjd/service/Saurabh_Service.java
@@ -2,8 +2,11 @@ package com.kjd.service;
 
 public class Saurabh_Service {
 
+	
+	String s="Santosh sir";
 	public static void main(String[] args) {
 		
 		System.out.println("Hi Sir ..I  am done Service Impl");
 	}
+	
 }
